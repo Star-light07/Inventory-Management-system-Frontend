cd9bf5615c9460ef65b82668611c29032e9a2d88
Starlight <89262876+Star-light07@users.noreply.github.com>
2022-12-01 10:05:21 +0200

    Update ProductSummary.js

--- src/components/product/productSummary/ProductSummary.js
+++ src/components/product/productSummary/ProductSummary.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react"
 import "./ProductSummary.scss"
-import { AiFillDollarCircle } from "react-icons/ai"
+import { FaMoneyBillAlt } from "react-icons/fa"
 import { BsCart4, BsCartX } from "react-icons/bs"
 import { BiCategory } from "react-icons/bi"
 import InfoBox from "../../infoBox/InfoBox"
@@ -15,7 +15,7 @@ import {
 } from "../../../redux/features/product/productSlice"
 
 // Icons
-const earningIcon = <AiFillDollarCircle size={40} color="#fff" />
+const earningIcon = <FaMoneyBillAlt size={40} color="#fff" />
 const productIcon = <BsCart4 size={40} color="#fff" />
 const categoryIcon = <BiCategory size={40} color="#fff" />
 const outOfStockIcon = <BsCartX size={40} color="#fff" />
